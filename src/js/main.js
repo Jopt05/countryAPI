a28b04164e6f12ed466c747690433527a4f4cf10
Jopt <jopt0598@hotmail.com>
2020-11-25 19:32:20 -0600

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,207 @@
+const apiURL    = 'https://restcountries.eu/rest/v2/alpha/',
+countries       = ['dza', 'alb', 'ala', 'afg', 'isl', 'bra', 'usa', 'deu'],
+
+items_Container = document.querySelector('.content__items'),
+selecFilter     = document.querySelector('select'),
+optionFilter    = document.querySelectorAll('option'),
+inputSearch     = document.querySelector('input'),
+darkModeToggle  = document.querySelector('.header-darkmode'),
+content         = document.querySelector('.content'),
+itemView        = document.querySelector('.itemView'),
+itemViewContent = document.querySelector('.itemView__content'),
+backButton      = document.querySelector('.backbutton'),
+body            = document.body;
+
+const createItem  = async( country ) => {
+
+    for(let country of countries){
+
+        let resp = await fetch( `${ apiURL }${country}` );
+        resp = await resp.json();
+
+        const div = document.createElement('div');
+        div.classList.add('content__items-item');
+        div.id = country;
+
+
+        const html = `
+            <img src="${ resp.flag }" alt=""  id="${country}">
+            <div class="content__items-item-description" id="${country}">
+                <p class="name" id="${country}"><b>${ resp.name }</b></p>
+                <p class="population" id="${country}"><b>Population: </b>${ resp.population.toLocaleString() }</p>
+                <p class="region" id="${country}"><b>Region: </b>${ resp.region }</p>
+                <p class="capital" id="${country}"><b>Capital: </b>${ resp.capital }</p>
+            </div>
+        `;
+
+        div.innerHTML = html;
+        items_Container.append(div);
+
+    }
+
+    console.log('Done1');
+
+}
+
+const deleteRepeatedSelect = async(  ) => {
+
+    let array = [],
+    arrayUnique = [];
+
+    for(let i = 0; i < countries.length - 1; i++){
+        let resp = await fetch( `${ apiURL }${countries[i]}` );
+        resp = await resp.json();
+        let region = resp.region;
+        array.push(region);
+    }
+
+    array.forEach( (repeated) => {
+        if( !arrayUnique.includes(repeated) ){
+            arrayUnique.push(repeated);
+        }
+    } )
+
+    arrayUnique.forEach( (aU) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', `${aU}`);
+
+        const html = `
+        ${aU}`;
+
+        option.innerHTML = html;
+        selecFilter.append(option);
+    } )
+
+    console.log('Done2');
+}
+
+const filterRegion = (value) => {
+    item = document.querySelectorAll('.content__items-item');
+    
+    if(value != 'all'){
+        for(let elem of item){
+            if(!elem.textContent.includes(value)){
+                elem.classList.add('hideItem');
+            } else if (elem.textContent.includes(value)) {
+                elem.classList.remove('hideItem');
+            }
+        }
+    } else {
+        for(let elem of item){
+            elem.classList.remove('hideItem');
+        }
+    }
+}
+
+const searchOnInput = (  ) => {
+    let value = inputSearch.value;
+    const pName    = document.querySelectorAll('.name');
+
+    if(value != ''){
+        for(let elem of pName){
+            if(!elem.textContent.includes(value)){
+                elem.parentElement.parentElement.classList.add('hideItem');
+            } else if (elem.textContent.includes(value)) {
+                elem.parentElement.parentElement.classList.remove('hideItem');
+            }
+        }
+    } else {
+        for(let elem of pName){
+            elem.parentElement.parentElement.classList.remove('hideItem');
+        }
+    }
+}
+
+const addListener = async() => {
+    const itemI = document.querySelectorAll('.content__items-item');
+    for(const item of itemI){
+        item.addEventListener('click', openDetails)
+    }
+}
+
+const openDetails = async(item) => {
+    let id = item.target.getAttribute('id');
+
+    // Creando HTML
+    let resp = await fetch( `${ apiURL }${id}` );
+        resp = await resp.json();
+
+        const html = `
+            <img src="${ resp.flag }" alt="">
+            <div class="itemView__content-data">
+                <h2>${ resp.name }</h2>
+                <div class="itemView__content-data-description">
+                    <div class="itemView__content-data-description1">
+                        <p><b>Native Name: </b>${ resp.nativeName }</p>
+                        <p><b>Poblation: </b>${ resp.population.toLocaleString() }</p>
+                        <p><b>Region: </b>${ resp.region }</p>
+                        <p><b>Sub-region: </b>${ resp.subregion }</p>
+                        <p><b>Capital: </b>${ resp.capital }</p>
+                    </div>
+                    <div class="itemView__content-data-description1">
+                        <p><b>Top Level Domain: </b>${ resp.topLevelDomain }</p>
+                        <p><b>Currencies: </b>${ resp.currencies[0].name }</p>
+                        <p><b>Languages: </b>${ resp.languages[0].name }</p>
+                    </div>
+                </div>
+                <p class="border"><b>Border Countries:</b></p>
+                
+            </div>
+        `;
+
+        itemViewContent.innerHTML = html;
+
+        await getBorders( resp.borders );
+    
+        content.classList.add('hide');
+        itemView.classList.remove('hide');
+}
+
+const getBorders = async( bordersArray ) => {
+    console.log(bordersArray);
+    const buttonContainer = document.querySelector('.itemView__content-data');
+
+    if( (bordersArray.length - 1) <= 1 ){
+        for(let i = 0; i <= 1; i++){
+            button = document.createElement('button');
+            button.classList.add('borderBtn')
+            button.innerHTML = bordersArray[i];
+            buttonContainer.append(button);
+        }
+    } else {
+        for(let i = 0; i <= 2; i++){
+            button = document.createElement('button');
+            button.classList.add('borderBtn')
+            button.innerHTML = bordersArray[i];
+            buttonContainer.append(button);
+            }
+    }
+}
+
+darkModeToggle.addEventListener('click', () => {
+    const darkModeDiv = document.querySelector('.header-darkmode');
+    
+    body.classList.toggle('darkMode');
+    darkModeDiv.classList.toggle('darkModeSpin');
+    
+    if( darkModeDiv.children[0].classList.contains('fas') ){
+        darkModeDiv.children[0].classList.remove('fas');
+        darkModeDiv.children[0].classList.add('far');
+    } else {
+        darkModeDiv.children[0].classList.add('fas');
+        darkModeDiv.children[0].classList.remove('far');
+    }
+});
+
+backButton.addEventListener('click', () => {
+    content.classList.remove('hide');
+    itemView.classList.add('hide');
+})
+
+inputSearch.addEventListener('keyup', searchOnInput)
+
+selecFilter.addEventListener('change', () => filterRegion(selecFilter.value));
+
+createItem().then( deleteRepeatedSelect )
+            .then( addListener );
+
